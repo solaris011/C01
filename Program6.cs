632ebcfceb43d9f9f1380420a1f50b651bd656b8
Saul F. <solaris0250@gmail.com>
2024-11-21 22:53:23 -0600

    Inicio

---
+++ Program6.cs
@@ -0,0 +1,18 @@
+﻿string? readResult;
+bool validEntry = false;
+
+do
+{
+    Console.Write("Enter your role name (Administrator, Manager, or User): ");
+    readResult = Console.ReadLine().Trim().ToLower();
+    
+    if (readResult == "administrator" || readResult == "manager" || readResult == "user")
+    {
+        validEntry = true;
+        Console.WriteLine($"Your input value ({readResult}) has been accepted.");        
+    }
+    else
+        Console.WriteLine("The role name that you entered, \"{readResult}\" is not valid. " +
+                         "Enter your role name (Administrator, Manager, or User)");
+
+} while (validEntry == false);
